3e2c32d84d5da0e176b5ff6a783adea072de5957
ezhilkannan <ezhilkannan7@gmail.com>
2022-09-12 03:06:11 +0530

    Add test cases and refactor

--- database/repo.py
+++ database/repo.py
@@ -3,27 +3,24 @@ import mysql.connector
 
 
 class TransactionsRepo:
-    def __init__(self, host: str, db_name: str, user_name: str, password: str):
+    def __init__(self, host: str, db_name: str, user_name: str, password: str, port: int):
         self._mysql_db = mysql.connector.connect(
             host=host,
             user=user_name,
             password=password,
-            database=db_name
+            database=db_name,
+            port=port
         )
         self._cursor = self._mysql_db.cursor()
     
     def _is_cyclic(self, transaction_id: int, parent_id: int):
-        query = f"""
-            with recursive cte as (
-            select * from Transactions where id={transaction_id}
-            union
-            select T.id, T.amount, T.parent_id from Transactions T join cte C on C.id=T.parent_id
-            )
-            select * from cte where T.id = {parent_id};
-        """
+        query = f"""WITH recursive cte AS 
+        (SELECT * from Transactions T where T.id=%s UNION ALL SELECT T.id, T.amount, 
+        T.parent_id from Transactions T join cte C on C.id=T.parent_id)
+        SELECT * from cte where T.id = %s;"""
         try:
             # checking if parent id is already a child of transaction id. to prevent cyclicity
-            data = self._cursor.execute(query)
+            data = self._cursor.execute(query, [transaction_id, parent_id])
             return len(data) == 0
         except Exception as e:
             logger.exception(f"Error occurred while fetching child transanction data for transaction_id: {transaction_id} due to {e}")
@@ -32,9 +29,9 @@ class TransactionsRepo:
     def create_transaction(self, transaction_id: int, amount: float, transaction_type: str, parent_id: int):
         if self._is_cyclic(transaction_id=transaction_id, parent_id=parent_id):
             raise ValueError("Invalid parentId provided")
-        query = f"INSERT INTO Transactions(id, amount, transaction_type, parent_id) values({transaction_id, amount, transaction_type, parent_id})"
+        query = f"INSERT INTO Transactions(id, amount, transaction_type, parent_id) values(%s, %s, %s, %s)"
         try:
-            self._cursor.execute(query)
+            self._cursor.execute(query, [transaction_id, amount, transaction_type, parent_id])
             self._mysql_db.commit()
         except Exception as e:
             logger.exception(f"Exception occurred while inserting into db for transaction_id: {transaction_id}")
@@ -52,18 +49,18 @@ class TransactionsRepo:
             raise Exception("Error occurred while inserting into db")
     
     def get_transactions_by_type(self, transaction_type: str):
-        query = "SELECT transaction_id, amount, transaction_type, parent_id from Transactions where transaction_type={transaction_type}"
+        query = "SELECT id, amount, transaction_type, parent_id from Transactions where transaction_type=%s"
         try:
-            self._cursor.execute(query)
+            self._cursor.execute(query, [transaction_type])
             return self._cursor.fetchall()
         except Exception as e:
             logger.exception(f"Error occurred while fetching data for transaction_type: {transaction_type} due to {e}")
             raise Exception("Error occurred")
 
     def get_transactions_by_id(self, transaction_id: int):
-        query = "SELECT transaction_id, amount, transaction_type, parent_id from Transactions where id={transaction_id}"
+        query = "SELECT id, amount, transaction_type, parent_id from Transactions where id=%s"
         try:
-            self._cursor.execute(query)
+            self._cursor.execute(query, [transaction_id])
             self._cursor.fetchall()
         except Exception as e:
             logger.exception(f"Error occurred while fetching data for transaction_id: {transaction_id} due to {e}")
---
+++ tests/test_transactions_repo.py
@@ -0,0 +1,82 @@
+import json
+import unittest
+import mysql
+import yaml
+from database.repo import TransactionsRepo
+
+import mysql.connector
+
+from utils import get_config
+
+db_config = get_config()
+
+
+class TestTransactionsRepo(unittest.TestCase):
+
+    # def __init__(self):
+    #     super().__init__()
+    #     self.transaction = TransactionsRepo(host=db_config["TEST_MYSQL_HOST"],
+    #                                         user_name=db_config["TEST_MYSQL_USER"],
+    #                                         password=db_config["TEST_MYSQL_PASSWORD"],
+    #                                         db_name=db_config["TEST_MYSQL_DB"],
+    #                                         port=db_config["TEST_MYSQL_PORT"])
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        cnx = mysql.connector.connect(
+            host=db_config["TEST_MYSQL_HOST"],
+            user=db_config["TEST_MYSQL_USER"],
+            password=db_config["TEST_MYSQL_PASSWORD"],
+            database=db_config["TEST_MYSQL_DB"],
+            port=db_config["TEST_MYSQL_PORT"]
+        )
+        cursor = cnx.cursor(dictionary=True)
+
+        query_1 = "DROP TABLE Transactions"
+        try:
+            cursor.execute(query_1)
+            cnx.commit()
+        except Exception as e:
+            print(e)
+
+        cursor = cnx.cursor(dictionary=True)
+
+        query_2 = """CREATE TABLE Transactions (
+                  id INTEGER NOT NULL PRIMARY KEY ,
+                  amount FLOAT,
+                  transaction_type VARCHAR(30) NOT NULL,
+                  parent_id INTEGER
+                )"""
+        try:
+            cursor.execute(query_2)
+            cnx.commit()
+        except mysql.connector.Error as err:
+            print(err.msg)
+        else:
+            print("OK")
+
+    def test_insert_transactions(self):
+        self.transaction = TransactionsRepo(host=db_config["TEST_MYSQL_HOST"],
+                                            user_name=db_config["TEST_MYSQL_USER"],
+                                            password=db_config["TEST_MYSQL_PASSWORD"],
+                                            db_name=db_config["TEST_MYSQL_DB"],
+                                            port=db_config["TEST_MYSQL_PORT"])
+
+        self.transaction.create_transaction(transaction_id=1, amount=100, parent_id=0, transaction_type="a")
+        self.transaction.create_transaction(transaction_id=2, amount=200, parent_id=1, transaction_type="b")
+        self.transaction.create_transaction(transaction_id=3, amount=300, parent_id=1, transaction_type="a")
+        self.transaction.create_transaction(transaction_id=4, amount=400, parent_id=2, transaction_type="c")
+        self.transaction.create_transaction(transaction_id=5, amount=500, parent_id=3, transaction_type="d")
+
+        out_1 = self.transaction.get_transactions_by_type(transaction_type="a")
+
+        self.assertEqual(len(out_1), 2)
+        self.assertEqual([1, 3], [i[0] for i in out_1])
+
+        self.transaction.create_transaction(transaction_id=6, amount=600, parent_id=5, transaction_type="e")
+        self.transaction.create_transaction(transaction_id=7, amount=700, parent_id=6, transaction_type="a")
+
+
+
+
+
