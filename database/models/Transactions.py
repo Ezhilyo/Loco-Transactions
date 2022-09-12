ca5d3a678af83ff99925ea90adfa7f1d0b28b16d
ezhilkannan <ezhilkannan7@gmail.com>
2022-09-12 11:22:30 +0530

    Updated Transactions

--- database/models/Transactions.py
+++ database/models/Transactions.py
@@ -1,5 +1,9 @@
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
 
 
-
-class Transactions():
-    
+class Transactions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.String(80), unique=True, nullable=False)
+    transaction_type = db.Column(db.String(30), unique=False, nullable=False)
+    parent_id = db.Column(db.Integer, nullable=True)
